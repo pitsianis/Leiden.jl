a4e475eb710bb095dbd3ba2f97176d655d57fc3f
Dimitris Floros <flojim4@gmail.com>
2022-11-10 09:35:30 -0500

    Added references in leiden.m

--- matlab/leiden.m
+++ matlab/leiden.m
@@ -1,7 +1,7 @@
 function [cid] = leiden(A, method, opt)
 % LEIDEN - Run Leiden for a specific gamma.
 %
-% CID = LEIDEN( A ) runs the Leiden algorithm, with the clustering
+% CID = LEIDEN( A ) runs the Leiden algorithm[1], with the clustering
 % function modularity, at γ = 1.0.
 %
 % CID = LEIDEN( A, CLUSTFUNC ) allows you to specify a different
@@ -25,6 +25,19 @@ function [cid] = leiden(A, method, opt)
 %    cid = leiden(A)
 %
 %
+%
+% ## References
+%
+% [1] V. A. Traag, L. Waltman and N. J. van Eck, [From Louvain to
+% Leiden: guaranteeing well-connected
+% communities](https://www.nature.com/articles/s41598-019-41695-z/),
+% Scientific Reports volume 9, 2019
+%
+% [2] T. Liu, D. Floros, N. Pitsianis and X. Sun, "[Digraph Clustering
+% by the BlueRed Method](https://ieeexplore.ieee.org/document/9622834),"
+% 2021 IEEE High Performance Extreme Computing Conference (HPEC), 2021,
+% pp. 1-7, doi: 10.1109/HPEC49654.2021.9622834.
+
 
 % Authors: Dimitris F., Nikos P., Xiaobai S.
 % Date: <Nov 10, 2022>
